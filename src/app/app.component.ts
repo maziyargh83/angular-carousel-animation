65e51a9151ea8de667d91fc5d537900c1e805b14
maziyargh83 <mazyar.gh83@gmail.com>
2021-08-22 02:12:20 +0430

    gh page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'carousel';
+  images: { src: string; active?: boolean }[] = [
+    {
+      src: 'https://picsum.photos/200',
+    },
+    {
+      src: 'https://picsum.photos/200',
+    },
+    {
+      src: 'https://picsum.photos/200',
+    },
+    {
+      src: 'https://picsum.photos/200',
+    },
+    {
+      src: 'https://picsum.photos/200',
+    },
+  ];
+  onSwiper(e: any) {
+    console.log(e);
+    this.images[e.activeIndex].active = true;
+  }
+  onSlideChange(w: any) {
+    if (w.swipeDirection == 'next') {
+      for (let i = 0; i <= w.activeIndex; i++) {
+        this.images[i].active = true;
+      }
+    } else if (w.swipeDirection == 'prev') {
+      for (let i = w.activeIndex; i < this.images.length; i++) {
+        this.images[i].active = false;
+      }
+    }
+  }
+
+  garr(length: number) {
+    return new Array(length);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SwiperModule } from 'swiper/angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SwiperModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
